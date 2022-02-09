4ac0a7b44f4d77ecb2390ab36eb9e343a774eb68
Omer Mustel <76651649+Omer-Mus@users.noreply.github.com>
2022-02-08 23:30:16 -0500

    Update spring.h

--- spring.h
+++ spring.h
@@ -1,27 +1,40 @@
 #include "mass.h"
-#include <cmath>  
+#include <cmath>
 #ifndef SPRING_H
 #define SPRING_H
 
 class Spring {
 public:
-  double L0 = 0;
-  double K = 10'000;
-  Mass m1 = Mass(0,0,0,0);
-  Mass m2 = Mass(0,0,0,0);
+    double L0 = 0;
+    double K = 10'000;
+    Mass m1 = Mass(0,0,0,0);
+    Mass m2 = Mass(0,0,0,0);
 
-  Spring(Mass m1, Mass m2, double K = 10'000) {
-    m1 = m1;
-    m2 = m2;
-    K = K;
-    L0 = sqrt(pow((m1.p[0] - m2.p[0]), 2) + pow(m1.p[1] - m2.p[1], 2) + pow(m1.p[2] - m2.p[2], 2));
-  }
-
-  double springForce() {
-    double L = sqrt(pow((m1.p[0] - m2.p[0]), 2) + pow(m1.p[1] - m2.p[1], 2) + pow(m1.p[2] - m2.p[2], 2));
-    return K * (L - L0);
-  }
-  ~Spring() {}
+    Spring(Mass m1, Mass m2, double K = 10'000) {
+        m1 = m1;
+        m2 = m2;
+        K = K;
+        L0 = sqrt(pow((m1.p[0] - m2.p[0]), 2) + pow(m1.p[1] - m2.p[1], 2) + pow(m1.p[2] - m2.p[2], 2));
+    }
+    void operator = (const Spring &S ) {
+        m1 = S.m1;
+        m2 = S.m2;
+        K = S.K;
+        L0 = S.L0;
+    }
+    double springForce() {
+        double L = sqrt(pow((m1.p[0] - m2.p[0]), 2) + pow(m1.p[1] - m2.p[1], 2) + pow(m1.p[2] - m2.p[2], 2));
+        return K * (L - L0);
+    }
+    std::vector<double> getVec1() {
+        std::vector<double> v(m1.p.begin(), m1.p.end());
+        return v;
+    }
+    std::vector<double> getVec2() {
+        std::vector<double> v(m2.p.begin(), m2.p.end());
+        return v;
+    }
+    ~Spring() {}
 
 };
 
