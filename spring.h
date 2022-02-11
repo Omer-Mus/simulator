f45a45d7b29df33add98b01529027eca69f05eda
Omer Mustel <76651649+Omer-Mus@users.noreply.github.com>
2022-02-10 20:33:31 -0500

    Update spring.h

--- spring.h
+++ spring.h
@@ -1,16 +1,18 @@
 #include "mass.h"
 #include <cmath>
+#include <iostream>
+
 #ifndef SPRING_H
 #define SPRING_H
 
 class Spring {
 public:
-    double L0 = 0;
-    double K = 10'000;
+    float L0 = 0;
+    float K = 10'000;
     Mass m1 = Mass(0,0,0,0);
     Mass m2 = Mass(0,0,0,0);
 
-    Spring(Mass m1, Mass m2, double K = 10'000) {
+    Spring(Mass m1, Mass m2, float K = 10'000) {
         m1 = m1;
         m2 = m2;
         K = K;
@@ -23,15 +25,15 @@ public:
         L0 = S.L0;
     }
     double springForce() {
-        double L = sqrt(pow((m1.p[0] - m2.p[0]), 2) + pow(m1.p[1] - m2.p[1], 2) + pow(m1.p[2] - m2.p[2], 2));
+        float L = sqrt(pow((m1.p[0] - m2.p[0]), 2) + pow(m1.p[1] - m2.p[1], 2) + pow(m1.p[2] - m2.p[2], 2));
         return K * (L - L0);
     }
-    std::vector<double> getVec1() {
-        std::vector<double> v(m1.p.begin(), m1.p.end());
+    std::vector<float> getVec1() {
+        std::vector<float> v(m1.p.begin(), m1.p.end());
         return v;
     }
-    std::vector<double> getVec2() {
-        std::vector<double> v(m2.p.begin(), m2.p.end());
+    std::vector<float> getVec2() {
+        std::vector<float> v(m2.p.begin(), m2.p.end());
         return v;
     }
     ~Spring() {}
