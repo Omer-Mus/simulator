6a741ad04b0cce6499b2108c0141964e2b97d69e
Omer Mustel <76651649+Omer-Mus@users.noreply.github.com>
2022-02-10 20:33:09 -0500

    Update mass.h

--- mass.h
+++ mass.h
@@ -1,4 +1,5 @@
 #include <vector>
+#include <cmath>
 
 #ifndef MASS_H
 #define MASS_H
@@ -10,70 +11,70 @@ If you want to include an update in as a member/class function, then you would p
 PS: If you don't use the mass.external variable, then get rid of it.
 */
 class Mass {
-  
+
 public:
-  double m=0;
-  std::vector<double> p; //position
-  std::vector<double> v; //velocity
-  std::vector<double> a; //acceleration
-  std::vector<double> F; //external forces
+    float m = 0;
+    std::vector<float> p; //position
+    std::vector<float> v; //velocity
+    std::vector<float> a; //acceleration
+    std::vector<float> F; //external forces
 
-  Mass(double x, double y, double z, double m) {
-    m = m;  // intialize mass
-    //set intial position
-    p.push_back(x);
-    p.push_back(y);
-    p.push_back(z);
+    Mass(float x, float y, float z, float mass) {
+        m = mass;  // intialize mass
+        //set intial position
+        p.push_back(x);
+        p.push_back(y);
+        p.push_back(z);
 
-    // set velocity, acceleration & Froce to 0
-    for (int i=0; i < 3; i ++) {
-      v.push_back(0);
-      a.push_back(0);
-      F.push_back(0); // F = a*m
+        // set velocity, acceleration & Froce to 0
+        for (int i=0; i < 3; i ++) {
+            v.push_back(0);
+            a.push_back(0);
+            F.push_back(0); // F = a*m
+        }
+        // intially, gravitiy is the acceleration
     }
-    // intially, gravitiy is the acceleration
-  }
 
-  ~Mass(){ 
+    ~Mass(){
 
-  }
-  
-  // assigment operator overload
-  void operator = (const Mass &M ) { 
-    m = M.m;
-    p = M.p;
-    a = M.a;
-    v = M.v;
-    F = M.F;
-  }
-
-  // calculate force
-  void init_force(int x, int y, int z) {
-    F.push_back(x*m);
-    F.push_back(y*m);
-    F.push_back(z*m);
-  }
-
-  // update velocity
-  void update_velocity() {
-    for (int i = 0; i < 3; i++ ) {
-      v[i] = v[i] + DT*a[i];
     }
-  }
 
-  // update position 
-  void update_position(const double DT) {
-    for (int i = 0; i < 3; i++ ) {
-      p[i] = p[i] + DT*v[i];
+    // assigment operator overload
+    void operator = (const Mass &M ) {
+        m = M.m;
+        p = M.p;
+        a = M.a;
+        v = M.v;
+        F = M.F;
     }
-  }
 
-  // calculate
-  void update_acceleration(const double DT) {
-    for (int i = 0; i < 3; i++ ) {
-      a[i] = F[i]/m;
+    // calculate force
+    void init_force(int x, int y, int z) {
+        F.push_back(x*m);
+        F.push_back(y*m);
+        F.push_back(z*m);
     }
-  }
+
+    // update velocity
+    void update_velocity(const float DT) {
+        for (int i = 0; i < 3; i++ ) {
+            v[i] = v[i] + DT*a[i];
+        }
+    }
+
+    // update position
+    void update_position(const float DT) {
+        for (int i = 0; i < 3; i++ ) {
+            p[i] = p[i] + DT*v[i];
+        }
+    }
+
+    // calculate
+    void update_acceleration() {
+        for (int i = 0; i < 3; i++ ) {
+            a[i] = round(F[i]/m);
+        }
+    }
 
 };
 
