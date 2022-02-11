016ca1ffe1d4c94bdcc1d13fd30eab0b09d41a6e
Omer Mustel <76651649+Omer-Mus@users.noreply.github.com>
2022-02-11 08:08:33 -0500

    Create vis.py

---
+++ vis.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import pandas as pd
+
+mass1 = '/Users/omeromer/CLionProjects/simulation/cmake-build-debug/mass1.csv'
+mass2 = '/Users/omeromer/CLionProjects/simulation/cmake-build-debug/mass2.csv'
+
+my_data = np.genfromtxt(mass1, delimiter=",", dtype=float)
+my_data2 = np.genfromtxt(mass2, delimiter=",", dtype=float)
+
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+# ax.plot(x,y,z)
+
+plt.plot(my_data, color = 'r')
+plt.plot(my_data2, color = 'g')
+plt.show()
