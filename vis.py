47f091697b2e149f28aba835ee965e3326613ef4
Omer Mustel <76651649+Omer-Mus@users.noreply.github.com>
2022-02-17 10:58:03 -0500

    Update vis.py

--- vis.py
+++ vis.py
@@ -1,9 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-import pandas as pd
+# from mpl_toolkits.mplot3d import Axes3D
+# import pandas as pd
+# import sys
 
-mass1 = '/Users/omeromer/CLionProjects/simulation/cmake-build-debug/mass1.csv' #change/delete path
+mass1 = '/Users/omeromer/CLionProjects/simulation/cmake-build-debug/mass1.csv'
 mass2 = '/Users/omeromer/CLionProjects/simulation/cmake-build-debug/mass2.csv'
 
 my_data = np.genfromtxt(mass1, delimiter=",", dtype=float)
